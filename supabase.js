a0eab6a355ac68d8933036330fdd5d12d460d47b
mercadodaniel521 <mercadodaniel521@gmail.com>
2025-03-22 19:28:47 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Inicializar el cliente de Supabase
-const supabaseUrl = 'https://okdvrezlnfdopzahepls.supabase.co';
+const supabaseUrl = 'https://okdvrezlnfdopzahepls.supabase.co/rest/v1/';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZHZyZXpsbmZkb3B6YWhlcGxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NDg4MTYsImV4cCI6MjA1MzQyNDgxNn0.NHZq5NSsp0uNZr8KHG-DMwns6nh3PKxo8w6LMox4Og8';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
